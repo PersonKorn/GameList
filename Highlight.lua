9358370fb0942c7c9ccf33516dcf0bfd5a6701b8
Korn <42650874+PersonKorn@users.noreply.github.com>
2025-01-05 01:16:22 +0000

    Update Highlight.lua

--- Highlight.lua
+++ Highlight.lua
@@ -46,9 +46,9 @@ local function highlightPlayer(player, highlightType)
 
     local settings = HIGHLIGHT_SETTINGS[highlightType] or {}
     highlight.FillColor = settings.FillColor or Color3.new(1, 1, 1) -- Default to white if not set
-    highlight.FillTransparency = 0.95 -- Set to a high value for barely visible fill
+    highlight.FillTransparency = 0.7 -- Set to a high value for barely visible fill
     highlight.OutlineColor = settings.HighlightColor or Color3.new(1, 1, 1) -- Default to white if not set
-    highlight.OutlineTransparency = 0.95 -- Set to a high value for barely visible outline
+    highlight.OutlineTransparency = 0.7 -- Set to a high value for barely visible outline
 end
 
 RunService.Heartbeat:Connect(function()
